add67262341589ae5085ace25dca800509f34817
Julian Lopez <julian.lopez@estudiants.urv.cat>
2019-01-13 17:05:10 +0100

    Se añade Pedidos

---
+++ database/migrations/2019_01_13_155327_create_orders_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('order_id');
+            $table->foreign('order_id')->references('id')->on('orders');            
+            $table->unsignedInteger('client_id');
+            $table->foreign('client_id')->references('id')->on('clients');            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
